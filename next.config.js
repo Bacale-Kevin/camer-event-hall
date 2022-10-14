3288eac16d53c875ea363f57f1a731a49d987df2
bacale <bacale86@gmail.com>
2022-10-14 13:58:29 +0100

    feat: added next-cors middleware

--- next.config.js
+++ next.config.js
@@ -1,6 +1,24 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
+  // async headers() {
+  //   return [
+  //     {
+  //       // matching all API routes
+  //       source: "/api/(.*)",
+  //       headers: [
+  //         { key: "Access-Control-Allow-Credentials", value: "true" },
+  //         { key: "Access-Control-Allow-Origin", value: "*" },
+  //         { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
+  //         {
+  //           key: "Access-Control-Allow-Headers",
+  //           value:
+  //             "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
+  //         },
+  //       ],
+  //     },
+  //   ];
+  // },
   webpack(config) {
     config.module.rules.push({
       test: /\.svg$/,
@@ -13,7 +31,7 @@ module.exports = {
     return [
       {
         source: "/api/:path*",
-        destination: "https://camer-event-hall-jgvb5im6h-bacale-kevin.vercel.app/:path*",
+        destination: "https://camer-event-hall-lpof9yh5v-bacale-kevin.vercel.app/:path*",
       },
     ];
   },
